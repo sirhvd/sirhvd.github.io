832a379eac9fd35438fe2afd1e947f41fb1e4a00
SIRHVD <huyvd147@gmail.com>
2026-04-10 21:33:58 +0700

    Update taihen_image_download.meta.js

--- taihen_image_download.meta.js
+++ taihen_image_download.meta.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name        Taihen Image Download
 // @namespace   TaihenImageDownload
-// @match       https://hentaiforce.net/view/*
-// @match       https://nhentai.com/*/comic/*
-// @exclude     https://nhentai.com/*/comic/*/reader/*
-// @match       https://nhentai.xxx/g/*
-// @match       https://*.hentai.name/g/*
-// @match       https://allporncomic.com/porncomic/*/*
+// @match       https://hentaiforce.net/*
+// @match       https://nhentai.com/*
+// @match       https://*.hentai.name/*
+// @match       https://nhentai.net/*
+// @match       https://nhentai.xxx/*
+// @match       https://allporncomic.com/*
 // @grant       GM_xmlhttpRequest
-// @connect     *
-// @version     1.2.1
+// @version     1.3
 // @require     https://cdn.jsdelivr.net/npm/fflate@0.8.2/umd/index.js
 // @downloadURL https://raw.githubusercontent.com/sirhvd/sirhvd.github.io/refs/heads/main/taihen_image_download.user.js
 // @updateURL   https://raw.githubusercontent.com/sirhvd/sirhvd.github.io/refs/heads/main/taihen_image_download.meta.js
