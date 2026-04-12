378376cc2ac3efe74563aea47ff7e7965183d569
SIRHVD <huyvd147@gmail.com>
2026-04-13 00:22:35 +0700

    Update taihen_image_download.meta.js

--- taihen_image_download.meta.js
+++ taihen_image_download.meta.js
@@ -7,8 +7,9 @@
 // @match       https://nhentai.net/*
 // @match       https://nhentai.xxx/*
 // @match       https://allporncomic.com/*
+// @match       https://kissmanga.in/kissmanga/*/*/
 // @grant       GM_xmlhttpRequest
-// @version     1.3
+// @version     1.4
 // @require     https://cdn.jsdelivr.net/npm/fflate@0.8.2/umd/index.js
 // @downloadURL https://raw.githubusercontent.com/sirhvd/sirhvd.github.io/refs/heads/main/taihen_image_download.user.js
 // @updateURL   https://raw.githubusercontent.com/sirhvd/sirhvd.github.io/refs/heads/main/taihen_image_download.meta.js
